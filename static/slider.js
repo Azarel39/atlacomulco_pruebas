7356f553d5515f29658548a840b5eb8ca6a1be8c
angelsaez20boee <angel.flores.sanchez@hotmail.com>
2025-03-24 12:01:32 -0600

    slider

--- static/slider.js
+++ static/slider.js
@@ -1,3 +1,5 @@
+// INICIO CÓDIGO JS DEL SLIDER
+
 document.addEventListener("DOMContentLoaded", function () {
     let index = 0;
     const slides = document.querySelectorAll(".slide");
@@ -6,9 +8,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Función para mostrar el slide actual
     function mostrarSlide(n) {
+        // Oculta todas las imágenes
         slides.forEach(slide => slide.classList.remove("active"));
+
+        // Corrige el índice si es necesario
         if (n >= slides.length) index = 0;
         if (n < 0) index = slides.length - 1;
+
+        // Muestra la imagen correspondiente
         slides[index].classList.add("active");
     }
 
@@ -22,11 +29,14 @@ document.addEventListener("DOMContentLoaded", function () {
     prevButton.addEventListener("click", () => moverSlide(-1));
     nextButton.addEventListener("click", () => moverSlide(1));
 
-    // Cambio automátic
+    // Cambio automático cada 3 segundos
     setInterval(() => {
         moverSlide(1);
     }, 3000);
+
     // Muestra la primera imagen al cargar la página
     mostrarSlide(index);
 });
+
+// FIN CÓDIGO JS DEL SLIDER
 
